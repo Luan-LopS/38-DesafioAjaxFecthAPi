7c28103a0a9bb5597e0d1b1950454813dc603b62
Luan <luam10@hotmail.com.br>
2024-10-23 17:13:43 -0300

    git init

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    const avatar = document.querySelector('#avatar')
+    const name = document.querySelector('#name')
+    const userName = document.querySelector('#userName')
+    const repository = document.querySelector('#repository')
+    const follower = document.querySelector('#follower')
+    const following = document.querySelector('#following')
+    const link = document.querySelector('#link')
+
+    const endpoint = `https://api.github.com/users/Luan-LopS`
+
+    fetch(endpoint)
+        .then(function(e){
+            return e.json()
+        }).then(function(json){
+            console.log(json)
+            avatar.src = json.avatar_url
+            name.innerText = json.name
+            userName.innerText = '@'+json.login
+            repository.innerText = json.public_repos
+            follower.innerText = json.followers
+            following.innerText = json.following
+            link.href = json.html_url
+        })
+})
