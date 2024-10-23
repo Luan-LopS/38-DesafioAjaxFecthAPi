d2e12ca190da31173f8a4613dc9a7ce047ef2b97
Luan <luam10@hotmail.com.br>
2024-10-23 17:19:32 -0300

    adicionado try, catch e animado o tamanho do botão de link

--- main.js
+++ main.js
@@ -9,17 +9,23 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     const endpoint = `https://api.github.com/users/Luan-LopS`
 
-    fetch(endpoint)
-        .then(function(e){
-            return e.json()
-        }).then(function(json){
-            console.log(json)
-            avatar.src = json.avatar_url
-            name.innerText = json.name
-            userName.innerText = '@'+json.login
-            repository.innerText = json.public_repos
-            follower.innerText = json.followers
-            following.innerText = json.following
-            link.href = json.html_url
-        })
+    try{
+        fetch(endpoint)
+            .then(function(e){
+                return e.json()
+            }).then(function(json){
+                console.log(json)
+                avatar.src = json.avatar_url
+                name.innerText = json.name
+                userName.innerText = '@'+json.login
+                repository.innerText = json.public_repos
+                follower.innerText = json.followers
+                following.innerText = json.following
+                link.href = json.html_url
+            })
+    }catch(error){
+        throw new error('Não encontrado!!')
+    }
+        
+
 })
